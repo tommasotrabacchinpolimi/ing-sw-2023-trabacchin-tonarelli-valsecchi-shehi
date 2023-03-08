a8c51d3a39dbd15d094cfabd7d71fc61971c29a9
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2023-03-08 11:03:22 +0100

    Aggiunte classi Player, Board, BoardSquare

---
+++ src/main/java/model/BoardSquare.java
@@ -0,0 +1,7 @@
+package model;
+
+public class BoardSquare {
+    private BoardSquare[] linkedBoardSquares;
+    private Tile tile;
+
+}
--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,5 +1,19 @@
 package model;
 
 public class Player {
-    String name;
+    private String nickName;
+    private PersonalGoal personalGoal;
+
+    public Player(String  nickName, PersonalGoal personalGoal){
+        this.nickName = nickName;
+        this.personalGoal = personalGoal;
+    }
+
+    public String getNickName(){
+        return nickName;
+    }
+
+    public PersonalGoal getPersonalGoal() {
+        return personalGoal;
+    }
 }
