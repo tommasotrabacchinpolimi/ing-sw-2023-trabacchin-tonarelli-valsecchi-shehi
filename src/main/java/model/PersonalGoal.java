2384a773718e26aaa5ea6b158e3704c28c883a1f
Melanie <melanietonarelli@yahoo.it>
2023-03-08 11:21:58 +0100

    aggiunta metodi get e set in CommonGoal e PersonalGoal su model

--- src/main/java/model/PersonalGoal.java
+++ src/main/java/model/PersonalGoal.java
@@ -2,4 +2,14 @@ package model;
 
 public class PersonalGoal {
     private int[][] referenceMatrix;
+
+    //costruttore
+    public PersonalGoal(int[][] matrix){
+        referenceMatrix = matrix;
+    }
+
+    public int[][] getReferenceMatrix() {
+        return referenceMatrix;
+    }
+
 }
