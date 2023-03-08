ef757a957f74c77c2263eba75d62fd5f851d637a
Melanie <melanietonarelli@yahoo.it>
2023-03-08 11:01:46 +0100

    Aggiunta classi CommonGoal e PersonalGoal Model

---
+++ src/main/java/model/CommonGoal.java
@@ -0,0 +1,22 @@
+package model;
+
+public enum CommonGoal{
+    //nomi da assegnare in modo più significativo
+    COMMON_GOAL1,
+    COMMON_GOAL2,
+    COMMON_GOAL3,
+    COMMON_GOAL4,
+    COMMON_GOAL5,
+    COMMON_GOAL6,
+    COMMON_GOAL7,
+    COMMON_GOAL8,
+    COMMON_GOAL9,
+    COMMON_GOAL10,
+    COMMON_GOAL11,
+    COMMON_GOAL12;
+
+    //attributo per memorizzare il punteggio assegnato dal goal
+    private int availableScore;
+    //descrizione del gioco
+    private String description;
+}
