6eb206bbc661e0c3039af637f02eaf69f935bdd5
Emanuele Valsecchi <emanuelevalsecchi00@gmail.com>
2023-03-08 12:08:54 +0100

    Create le classi BookShelf.java, Tile.java e TileType.java con i metodi setter e getter

--- src/main/java/model/Tile.java
+++ src/main/java/model/Tile.java
@@ -1,4 +1,38 @@
 package model;
 
 public class Tile {
+    private TileType type;
+    private int subType;
+
+    public Tile(){
+        type = null;
+        subType = 0;
+    }
+
+    public Tile(Tile t){
+        this.type = t.getType();
+        this.subType = t.getSubType();
+    }
+
+    public Tile(TileType type, int subType) {
+        this.type = type;
+        this.subType = subType;
+    }
+
+    public void setType(TileType type ){
+        this.type = type;
+    }
+
+    public TileType getType() {
+        return type;
+    }
+
+    public void setSubType(int subType ){
+        this.subType = subType;
+    }
+
+    public int getSubType() {
+        return subType;
+    }
+
 }
