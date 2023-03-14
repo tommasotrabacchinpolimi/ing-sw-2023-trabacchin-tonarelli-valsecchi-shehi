4c3fb256f03710c726402ced45b54d838e01a1df
Emanuele Valsecchi <emanuele1.valsecchi@mail.polimi.it>
2023-03-14 15:00:42 +0100

    Aggiustata la sintassi come specificata su Drive

--- src/main/java/controller/CommonGoalFunction.java
+++ src/main/java/controller/CommonGoalFunction.java
@@ -6,87 +6,87 @@ public enum CommonGoalFunction {
 
     CG1_FUNCTION{
         @Override
-        public boolean execute(BookShelf bookShelf){
+        public boolean execute( BookShelf bookShelf ){
             return true;
         }
     },
 
     CG2_FUCNTION{
         @Override
-        public boolean execute(BookShelf bookShelf){
+        public boolean execute( BookShelf bookShelf ){
             return true;
         }
     },
 
     CG3_FUNCTION{
         @Override
-        public boolean execute(BookShelf bookShelf){
+        public boolean execute( BookShelf bookShelf ){
             return true;
         }
     },
 
     CG4_FUNCTION{
         @Override
-        public boolean execute(BookShelf bookShelf){
+        public boolean execute( BookShelf bookShelf ){
             return true;
         }
     },
 
     CG5_FUNCTION{
         @Override
-        public boolean execute(BookShelf bookShelf){
+        public boolean execute( BookShelf bookShelf ){
             return true;
         }
     },
 
     CG6_FUNCTION{
         @Override
-        public boolean execute(BookShelf bookShelf){
+        public boolean execute( BookShelf bookShelf ){
             return true;
         }
     },
 
     CG7_FUNCTION{
         @Override
-        public boolean execute(BookShelf bookShelf){
+        public boolean execute( BookShelf bookShelf ){
             return true;
         }
     },
 
     CG8_FUCNTION{
         @Override
-        public boolean execute(BookShelf bookShelf){
+        public boolean execute( BookShelf bookShelf ){
             return true;
         }
     },
 
     CG9_FUNCTION{
         @Override
-        public boolean execute(BookShelf bookShelf){
+        public boolean execute( BookShelf bookShelf ){
             return true;
         }
     },
 
     CG10_FUNCTION{
         @Override
-        public boolean execute(BookShelf bookShelf){
+        public boolean execute( BookShelf bookShelf ){
             return true;
         }
     },
 
     CG11_FUCNTION{
         @Override
-        public boolean execute(BookShelf bookShelf){
+        public boolean execute( BookShelf bookShelf ){
             return true;
         }
     },
 
     CG12_FUNCTION{
         @Override
-        public boolean execute(BookShelf bookShelf){
+        public boolean execute( BookShelf bookShelf ){
             return true;
         }
     };
 
-    abstract boolean execute(BookShelf bookShelf);
+    abstract boolean execute( BookShelf bookShelf );
 }
