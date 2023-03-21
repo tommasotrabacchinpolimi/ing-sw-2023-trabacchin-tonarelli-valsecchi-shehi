303043d4b3db512fa469c77184bfdfa1575ecf8e
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2023-03-21 22:11:04 +0100

    Implemented GroupCommonGoal

--- src/main/java/model/GroupCommonGoal.java
+++ src/main/java/model/GroupCommonGoal.java
@@ -1,12 +1,15 @@
 package model;
 
 import java.io.Serializable;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GroupCommonGoal extends CommonGoal implements Serializable {
     private static final long serialVersionUID = 98478359933L;
     private int groupsNumber;
     private int adjacentTiles;
+
+
 
     public GroupCommonGoal(int groupsNumber, int adjacentTiles){
         this.groupsNumber = groupsNumber;
@@ -31,13 +34,52 @@ public class GroupCommonGoal extends CommonGoal implements Serializable {
 
     @Override
     public List<EntryPatternGoal> rule(TileType[][] bookShelf){
-        int occurencyTile = 0;
-        int numberGrourps = 0;
+        Set<Set<EntryPatternGoal>> groups = findGroups(bookShelf);
+        Set<Set<EntryPatternGoal>> candidateGroups = groups.stream().filter(g -> g.size()==adjacentTiles).collect(Collectors.toSet());
+        if(candidateGroups.size()>=groupsNumber)
+        {
+            return candidateGroups.stream().flatMap(Collection::stream).collect(Collectors.toList());
+        }
+        else{
+            return null;
+        }
+    }
+    private Set<Set<EntryPatternGoal>> findGroups(TileType[][] bookShelf){
+        boolean[][] alreadyTaken = new boolean[bookShelf.length][bookShelf[0].length];//initialized to false
+        Set<Set<EntryPatternGoal>> result = new HashSet<Set<EntryPatternGoal>>();
+        for(int i = 0;i<bookShelf.length;i++){
+            for(int j = 0;j<bookShelf[0].length;j++){
+                findSingleGroup(i,j,bookShelf,alreadyTaken,bookShelf[i][j]).ifPresent(result::add);
+            }
+        }
+        return result;
+    }
 
-        for (int i = 0; i < bookShelf.length; i++){
-
+    private Optional<Set<EntryPatternGoal>> findSingleGroup(int i, int j, TileType[][] bookShelf, boolean[][] alreadyTaken, TileType tileType){
+        if (i<0||i>=bookShelf.length||j<0||j>=bookShelf[0].length){// nothing is to be returned if arguments are illegal
+            return Optional.empty();
+        }
+        if (alreadyTaken[i][j]){ //if this bookShelf is already part of another group then it should not be considered for another group
+            return Optional.empty();
+        }
+        Set<EntryPatternGoal> result = new HashSet<>();// Java documentation recommends using HashSet, unless otherwise required
+        if (bookShelf[i][j]!=tileType){//we want only entries whose type is tileType
+            return Optional.empty();
+        }
+        else{
+            result.add(new EntryPatternGoal(j,i,tileType));//if the type is correct then the (i,j)-entry can be added to the group
+            alreadyTaken[i][j] = true;
         }
 
-        return null;
+        findSingleGroup(i-1,j,bookShelf,alreadyTaken,tileType).ifPresent(result::addAll);
+        findSingleGroup(i+1,j,bookShelf,alreadyTaken,tileType).ifPresent(result::addAll);
+        findSingleGroup(i,j-1,bookShelf,alreadyTaken,tileType).ifPresent(result::addAll);
+        findSingleGroup(i,j+1,bookShelf,alreadyTaken,tileType).ifPresent(result::addAll);
+        return Optional.of(result);
+
+
+
     }
 }
+
+
