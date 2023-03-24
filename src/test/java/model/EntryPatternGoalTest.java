795e31e35837d64f026e1fe1201c52c93a0879df
Melanie <melanie.tonarelli@mail.polimi.it>
2023-03-24 14:51:19 +0100

    Implementazione caso di test per PlayerTest e EntryPatternGoal

--- src/test/java/model/EntryPatternGoalTest.java
+++ src/test/java/model/EntryPatternGoalTest.java
@@ -2,19 +2,39 @@ package model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EntryPatternGoalTest {
 
     @Test
     void getColumn() {
+        int row, col;
+        Random rnd = new Random();
+        row = rnd.nextInt(6);
+        col = rnd.nextInt(5);
+        EntryPatternGoal cell = new EntryPatternGoal(col, row, TileType.CAT);
+        assertEquals(col, cell.getColumn());
     }
 
     @Test
     void getRow() {
+        int row, col;
+        Random rnd = new Random();
+        row = rnd.nextInt(6);
+        col = rnd.nextInt(5);
+        EntryPatternGoal cell = new EntryPatternGoal(col, row, TileType.CAT);
+        assertEquals(row, cell.getRow());
     }
 
     @Test
     void getTileType() {
+        int row, col;
+        Random rnd = new Random();
+        row = rnd.nextInt(6);
+        col = rnd.nextInt(5);
+        EntryPatternGoal cell = new EntryPatternGoal(col, row, TileType.CAT);
+        assertEquals(TileType.CAT, cell.getTileType());
     }
 }
