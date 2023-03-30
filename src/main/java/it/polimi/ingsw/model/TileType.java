0c85105cee73761c2c590f9c6565afbedfb3a01d
Emanuele Valsecchi <emanuele1.valsecchi@mail.polimi.it>
2023-03-30 15:08:56 +0200

    Added serialVersionUID for class EntryPatternGoal.java. Added a field abbreviation for TileType with its corresponding method to get and set (constructor). Created JavaDoc for TileType.java

--- src/main/java/it/polimi/ingsw/model/TileType.java
+++ src/main/java/it/polimi/ingsw/model/TileType.java
@@ -1,12 +1,49 @@
 package it.polimi.ingsw.model;
 
-import java.io.Serializable;
+/**
+ * This class represent the TileType associated with each Tile.<br>
+ * Please refer to
+ * <a href="https://www.craniocreations.it/storage/media/product_downloads/48/538/MyShelfie_Ruleboo_ENG_lowres_new.pdf">
+ *     Official RuleBook
+ * </a> to understand which type of Tile there are in the game.
+ *
+ * @see TileSubject
+ */
+public enum TileType {
+    CAT("C"), BOOK("B"), GAME("G"), FRAME("F"), TROPHY("T"), PLANT("P");
 
-public enum TileType implements Serializable {
-    CAT, BOOK, GAME, FRAME, TROPHY, PLANT;
+    /**
+     * This String represent the TileType with only one letter, that correspond to the first letter of each Tile type name
+     */
+    private final String abbreviation;
 
-    private static final long serialVersionUID = 863462839735L;
+    /**
+     * Constructor that sets the enum to the correct value
+     *
+     * @param abbreviation string that reppresent the single letter for each Tile type
+     */
+    TileType(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
 
+    /**
+     * This method is used to get the first letter of each Tile type name
+     *
+     * @return first letter of each Tile type name
+     *
+     * @see #abbreviation
+     */
+    public String toAbbreviationString(){
+        return this.abbreviation;
+    }
+
+    /**
+     * Get the name of each Tile type
+     * {@inheritDoc}
+     *
+     * @return name of each TileType
+     * @see TileType
+     */
     @Override
     public String toString() {
         return this.name();
