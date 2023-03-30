a1d4bb5b6082f633a0b67fd192adc1f0e082ae87
Emanuele Valsecchi <emanuele1.valsecchi@mail.polimi.it>
2023-03-30 14:44:51 +0200

    Removed deprecated method in test file PlayerTest.java

--- src/test/java/it/polimi/ingsw/model/PlayerTest.java
+++ src/test/java/it/polimi/ingsw/model/PlayerTest.java
@@ -21,21 +21,4 @@ class PlayerTest {
         Player player = new Player("Tommaso", goal);
         assertEquals(goal.toString(), player.getPersonalGoal().toString());
     }
-
-    @Test
-    void getScore() {
-        PersonalGoal goal = new PersonalGoal();
-        Player player = new Player("Emanuele", goal);
-        assertEquals(0, player.getScore());
-    }
-
-    @Test
-    void setScore() {
-        PersonalGoal goal = new PersonalGoal();
-        Random rnd = new Random();
-        int score = rnd.nextInt();
-        Player player = new Player("Adem", goal);
-        player.setScore(score);
-        assertEquals(score, player.getScore());
-    }
 }
