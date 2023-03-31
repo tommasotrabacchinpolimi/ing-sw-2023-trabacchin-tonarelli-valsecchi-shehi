6dba41074304eff45684ed126f38437a4fba48a5
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2023-03-31 16:08:57 +0200

    Implemented methods in GameManager

--- src/main/java/it/polimi/ingsw/controller/GameManager.java
+++ src/main/java/it/polimi/ingsw/controller/GameManager.java
@@ -1,15 +1,17 @@
 package it.polimi.ingsw.controller;
 
-import it.polimi.ingsw.model.BoardSquare;
-import it.polimi.ingsw.model.CommonGoal;
-import it.polimi.ingsw.model.Player;
-import it.polimi.ingsw.model.TileType;
+import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.net.RemoteInterface;
 import it.polimi.ingsw.net.User;
+
+import java.util.*;
+import java.util.function.Function;
 
 import static it.polimi.ingsw.model.BoardSquareType.THREE_DOTS;
 
-public class GameManager {
+public class GameManager<R extends RemoteInterface> {
     private Controller controller;
+    private Function<Integer,Integer> fromGroupSizeToScore;
 
     public Controller getController() {
         return controller;
@@ -52,5 +54,71 @@ public class GameManager {
                     b.getLeft().getTileSubject()!=null || b.getTop().getTileSubject()!=null) return false;
         }
         return true;
+    }
+
+    private void evaluateFinalScore(Player player){
+        int scoreAdjacentGoal = 0;
+        int personalGoalMatches = 0;
+        int scorePersonalGoal = 0;
+        TileType[][] bookShelf = player.getBookShelf().toTileTypeMatrix();
+        Set<Set<EntryPatternGoal>> groups = findGroups(bookShelf);
+        for(Set<EntryPatternGoal> group : groups){
+            scoreAdjacentGoal += fromGroupSizeToScore.apply(group.size());
+        }
+        for(EntryPatternGoal e : player.getPersonalGoal().getGoalPattern()){
+            if(player.getBookShelf().toTileTypeMatrix()[e.getRow()][e.getColumn()]==e.getTileType()){
+                personalGoalMatches++;
+            }
+        }
+        scorePersonalGoal = player.getPersonalGoal().getScoreMap().get(personalGoalMatches);
+        player.getPointPlayer().setScoreAdjacentGoal(scoreAdjacentGoal);
+        player.getPointPlayer().setScorePersonalGoal(scorePersonalGoal);
+    }
+
+    public void dragTilesToBookShelf(User<R> user, int[] chosenTiles, int chosenColumn){
+        Player player = getController().getState().getPlayers().stream().filter(p->p.getNickName().equals(user.getNickName())).toList().get(0);
+        Board board = getController().getState().getBoard();
+        List<TileSubject> tiles = new ArrayList<>();
+        for(Integer tile : chosenTiles){
+            tiles.add(board.fromIntToBoardSquare(tile).getTileSubject());
+        }
+        BookShelf bookShelf = player.getBookShelf();
+        bookShelf.addTileSubjectTaken(tiles,chosenColumn);
+    }
+    private Set<Set<EntryPatternGoal>> findGroups(TileType[][] bookShelf){
+        boolean[][] alreadyTaken = new boolean[bookShelf.length][bookShelf[0].length];//initialized to false
+        Set<Set<EntryPatternGoal>> result = new HashSet<Set<EntryPatternGoal>>();
+        for(int i = 0;i<bookShelf.length;i++){
+            for(int j = 0;j<bookShelf[0].length;j++){
+                findSingleGroup(i,j,bookShelf,alreadyTaken,bookShelf[i][j]).ifPresent(result::add);
+            }
+        }
+        return result;
+    }
+
+    private Optional<Set<EntryPatternGoal>> findSingleGroup(int i, int j, TileType[][] bookShelf, boolean[][] alreadyTaken, TileType tileType){
+        if(tileType==null){
+            return Optional.empty();
+        }
+        if (i<0||i>=bookShelf.length||j<0||j>=bookShelf[0].length){// nothing is to be returned if arguments are illegal
+            return Optional.empty();
+        }
+        if (alreadyTaken[i][j]){ //if this bookShelf is already part of another group then it should not be considered for another group
+            return Optional.empty();
+        }
+        Set<EntryPatternGoal> result = new HashSet<>();// Java documentation recommends using HashSet, unless otherwise required
+        if (bookShelf[i][j]!=tileType){//we want only entries whose type is tileType
+            return Optional.empty();
+        }
+        else{
+            result.add(new EntryPatternGoal(j,i,tileType));//if the type is correct then the (i,j)-entry can be added to the group
+            alreadyTaken[i][j] = true;
+        }
+
+        findSingleGroup(i-1,j,bookShelf,alreadyTaken,tileType).ifPresent(result::addAll);
+        findSingleGroup(i+1,j,bookShelf,alreadyTaken,tileType).ifPresent(result::addAll);
+        findSingleGroup(i,j-1,bookShelf,alreadyTaken,tileType).ifPresent(result::addAll);
+        findSingleGroup(i,j+1,bookShelf,alreadyTaken,tileType).ifPresent(result::addAll);
+        return Optional.of(result);
     }
 }
