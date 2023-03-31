312a513de23494e9f999baef1d0540d6a0fe9681
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2023-03-31 16:32:06 +0200

    Completed dragTilesToBookShelf method

--- src/main/java/it/polimi/ingsw/controller/GameManager.java
+++ src/main/java/it/polimi/ingsw/controller/GameManager.java
@@ -86,6 +86,13 @@ public class GameManager<R extends RemoteInterface> {
         }
         BookShelf bookShelf = player.getBookShelf();
         bookShelf.addTileSubjectTaken(tiles,chosenColumn);
+        verifyEndGame(user);
+        if(verifyRefillBoard()){
+            getController().getState().getBoard().refillBoard(getController().getState().getPlayersNumber());
+        }
+        evaluateFinalScore(player);
+        verifyCommonGoal(user);
+        setNextCurrentPlayer();
     }
 
 
