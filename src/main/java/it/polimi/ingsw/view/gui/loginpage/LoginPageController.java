f0d6aeb958e027ca4706e704a3313f11f8af0fc3
Emanuele Valsecchi <emanuele1.valsecchi@mail.polimi.it>
2023-05-02 12:05:31 +0200

    Minor changes to login page & delete the useless App class in src/main/java/it/polimi/ingsw

--- src/main/java/it/polimi/ingsw/view/gui/loginpage/LoginPageController.java
+++ src/main/java/it/polimi/ingsw/view/gui/loginpage/LoginPageController.java
@@ -7,14 +7,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.Bloom;
-import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
 import org.jetbrains.annotations.NotNull;
 
 
