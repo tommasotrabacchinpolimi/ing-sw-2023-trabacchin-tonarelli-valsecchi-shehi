b1ae300fae6aad1232e9cd862c5fe40155d02a2b
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2023-05-03 09:30:07 +0200

    Added Triple class in utils

---
+++ src/main/java/it/polimi/ingsw/utils/Triple.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.utils;
+
+public class Triple <V,T,E>{
+    private final V first;
+    private final T second;
+    private final E third;
+
+    public Triple(V first, T second, E third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public V getFirst() {
+        return first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public E third() {
+        return third;
+    }
+}
