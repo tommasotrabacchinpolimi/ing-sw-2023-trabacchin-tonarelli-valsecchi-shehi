94d74e8122748c00e6b007c6273c8ad87cff8bae
Adem Shehi <adem.shehi@mail.polimi.it>
2023-05-05 12:42:34 +0200

    Added Javadoc to UserAccepter

--- src/main/java/it/polimi/ingsw/net/UserAccepter.java
+++ src/main/java/it/polimi/ingsw/net/UserAccepter.java
@@ -1,8 +1,42 @@
 package it.polimi.ingsw.net;
 
+/**
+ * <p>This interface used to manage and notice if a potential user can play</p>
+ * <p>More precisely actions managed are:
+ * <ul>
+ *     <li>
+ *         {@linkplain #acceptUser(User) accept users}
+ *     </li>
+ *     <li>
+ *         {@linkplain #registerConnectionDownListener(User) to notice a loss connection}
+ *     </li>
+ * </ul>
+ * </p>
+ *
+ *
+ * @param <R> the type of the remote object
+ *
+ * @see RemoteInterface
+ * @see User
+ *
+ * @author Tommaso Trabacchin
+ * @author Melanie Tonarelli
+ * @author Emanuele Valsecchi
+ * @author Adem Shehi
+ * @version 1.0, 27/04/2023
+ */
 public interface UserAccepter<R extends RemoteInterface> {
 
+    /**
+     * <p>This method is used to judge if a {@linkplain User user} is accepted to play</p>
+     * @param user the player to be judged
+     * @return True if the user can play, False otherwise
+     */
     boolean acceptUser(User<R> user);
 
+    /**
+     * <p>This method is used to notice a loss connection of a {@linkplain User user}</p>
+     * @param user the player to be checked
+     */
     void registerConnectionDownListener(User<R> user);
 }
