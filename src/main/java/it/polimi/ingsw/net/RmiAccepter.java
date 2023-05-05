9453742926e93054b5eee7d5e3d166a831703550
Adem Shehi <adem.shehi@mail.polimi.it>
2023-05-05 17:23:41 +0200

    Javadoc changes in RmiAccepter

--- src/main/java/it/polimi/ingsw/net/RmiAccepter.java
+++ src/main/java/it/polimi/ingsw/net/RmiAccepter.java
@@ -27,46 +27,56 @@ import com.google.gson.reflect.TypeToken;
  */
 public class RmiAccepter<L extends RemoteInterface, R extends RemoteInterface> implements RemoteAccepterInterface {
     /**
-     * This field is used to manage and notice if a potential user can play
+     * <p>This field is a reference to an actor that manage and notice if a potential user can play</p>
+     *
+     * @see it.polimi.ingsw.controller.LobbyController
      */
     private final UserAccepter<R> userAccepter;
 
     /**
-     * <p>This field is a local target object to invoke correct actions</p>
+     * <p>This field is a local target object on which invoke actions</p>
+     *
+     * @see it.polimi.ingsw.controller.Dispatcher
      */
     private final Object localTarget;
 
     /**
      * <p>Represents a generic type {@linkplain RemoteInterface remote object}</p>
-     * <p>Forces clients to create a subclass of this class which enables retrieval the type information even at runtime</p>
+     * <p>Forces clients to create a subclass of this class which enables retrieval the type information
+     *    even at runtime</p>
      */
     private final TypeToken<R> remoteTargetClass;
 
     /**
      * <p>Represents a generic type {@linkplain RemoteInterface local object}</p>
-     * <p>Forces clients to create a subclass of this class which enables retrieval the type information even at runtime</p>
+     * <p>Forces clients to create a subclass of this class which enables retrieval the type information even at
+     *    runtime</p>
      */
     private final TypeToken<L> localTargetClass;
 
     /**
-     * <p>This parameter specifies the port for connection</p>
+     * <p>This parameter specifies the port number for connection</p>
      */
     private final int portNumber;
 
     /**
-     * <p>supplier responsible for adapting users to the local target object</p>
+     * <p>{@link Supplier} responsible for adapting users to the local target object</p>
+     * <p>Thanks to this supplier an {@linkplain UserAdapterInterface user adapter interface} is generated "lazily"
+     * and asynchronously</p>
+     *
+     * @see UserAdapterInterface
      */
     private final Supplier<UserAdapterInterface<R>> userAdapterSupplier;
 
-
     /**
+     * Constructor
      *
-     * @param portNumber this is the port number used for connection
-     * @param userAccepter Object to manage and notice if a potential user can play
-     * @param localTarget local target object
-     * @param remoteTargetClass Represents a generic type {@linkplain RemoteInterface remote object}
-     * @param localTargetClass Represents a generic type {@linkplain RemoteInterface local object}
-     * @param executorService The executor service for the asynchronous execution
+     * @param portNumber this is the {@linkplain #portNumber port number} used for connection
+     * @param userAccepter {@linkplain #userAccepter manager} for potential user
+     * @param localTarget {@linkplain #localTarget local target} for actions
+     * @param remoteTargetClass represents a generic {@linkplain RemoteInterface remote} type
+     * @param localTargetClass represents a generic {@linkplain RemoteInterface local} type
+     * @param executorService the executor service for the asynchronous execution
      */
     public RmiAccepter(int portNumber, UserAccepter<R> userAccepter, Object localTarget, TypeToken<R> remoteTargetClass, TypeToken<L> localTargetClass, ExecutorService executorService){
         this.localTarget = localTarget;
@@ -97,33 +107,66 @@ public class RmiAccepter<L extends RemoteInterface, R extends RemoteInterface> i
     }
 
     /**
+     * {@inheritDoc}
+     * <p>More precisely this method:
+     * <ul>
+     *     <li>registers a new {@linkplain User user} to an RMI connection</li>
+     *     <li>setup the {@linkplain #localTarget local target} on which the user will execute action</li>
+     *     <li>a controller for the connection loss between {@linkplain User user} and server is setup</li>
+     * </ul>
+     * </p>
+     * @param remoteObject {@inheritDoc}
+     * @return <p>{@code null} if the {@linkplain User user} can't play (refer to
+     * {@link it.polimi.ingsw.controller.LobbyController#acceptUser(User u)} for more information).</p>
+     * <p>{@linkplain RemoteInterface remote proxy object} if the {@linkplain User user} can play, in this last case
+     * the RMI connection is also set up</p>
      *
-     * @param remoteObject
-     * @return
-     * @throws RemoteException
-     * @throws IOException
-     * @throws ClassNotFoundException
+     * @throws RemoteException {@inheritDoc}
+     * @throws IOException {@inheritDoc}
+     * @throws ClassNotFoundException {@inheritDoc}
+     *
+     * @see User
+     * @see RemoteInterface
+     * @see RemoteAccepterInterface
      */
     @Override
-    public RemoteInterface register(RemoteInterface remoteObject) throws RemoteException, IOException, ClassNotFoundException {
+    public RemoteInterface register(RemoteInterface remoteObject)
+            throws RemoteException, IOException, ClassNotFoundException {
+
         User<R> user = new User<R>();
+
         if(userAccepter.acceptUser(user)){
-            RmiConnectionManager<L,R> rmiConnectionManager = new RmiConnectionManager<>();
-            if(userAdapterSupplier==null){
-                rmiConnectionManager.init(portNumber,user,remoteTargetClass,localTargetClass,(L)localTarget,remoteObject);
-            }
-            else{
+            // If the user can play, an instance of connection manager is created for that player
+            RmiConnectionManager<L, R> rmiConnectionManager = new RmiConnectionManager<>();
+
+            if(userAdapterSupplier == null) {
+                // If the supplier is not created yet, a new RMI connection is set up
+                rmiConnectionManager.init(portNumber, user, remoteTargetClass, localTargetClass, (L)localTarget, remoteObject);
+            } else {
+                /* If the supplier has been created in the past, the UserAdapterInterface is retrieved from the supplier
+                 * At the UserAdapterInterface instance is connected the user created and the local target
+                 * on which call action is bound.
+                 * A new local target proxy is also created that will handle new calls coming from the new user added.
+                 * In the end, a new RMI connection manager creates a new connection
+                 */
                 UserAdapterInterface<R> userAdapterInterface = userAdapterSupplier.get();
                 userAdapterInterface.setUser(user);
                 userAdapterInterface.setTarget(localTarget);
-                L newLocalTarget = (L) Proxy.newProxyInstance(localTargetClass.getRawType().getClassLoader(),new Class[]{localTargetClass.getRawType()},userAdapterInterface);
-                rmiConnectionManager.init(portNumber,user,remoteTargetClass,localTargetClass,newLocalTarget,remoteObject);
+
+                /* Returns a proxy instance for the specified interfaces that dispatches method invocations to
+                 * the specified invocation handler.
+                 */
+                L newLocalTarget = (L) Proxy.newProxyInstance(localTargetClass.getRawType().getClassLoader(),
+                        new Class[]{localTargetClass.getRawType()}, userAdapterInterface);
+
+                rmiConnectionManager.init(portNumber, user, remoteTargetClass, localTargetClass, newLocalTarget, remoteObject);
             }
+
             user.setConnectionManager(rmiConnectionManager);
             userAccepter.registerConnectionDownListener(user);
+
             return rmiConnectionManager.getLocalRemoteObject();
-        }
-        else{
+        } else {
             return null;
         }
     }
