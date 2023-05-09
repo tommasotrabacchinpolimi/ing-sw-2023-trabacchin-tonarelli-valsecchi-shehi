30bc69c2604f2489ca3fabe7e5616444a098a763
Emanuele Valsecchi <emanuele1.valsecchi@mail.polimi.it>
2023-05-09 14:42:46 +0200

    Added JavaDoc for MyShelfieApplication

--- src/main/java/it/polimi/ingsw/view/gui/MyShelfieApplication.java
+++ src/main/java/it/polimi/ingsw/view/gui/MyShelfieApplication.java
@@ -11,33 +11,100 @@ import javafx.stage.Stage;
 import java.awt.*;
 import java.io.IOException;
 
+/**
+ * <p>This class is used to define the default path and commands allowed by every class that has to deal with the graphics
+ * interface</p>
+ *
+ * @see Application
+ *
+ * @author Tommaso Trabacchin
+ * @author Melanie Tonarelli
+ * @author Emanuele Valsecchi
+ * @author Adem Shehi
+ * @version 1.0
+ * @since 02/05/2023
+ */
 public abstract class MyShelfieApplication extends Application{
+
+    /**
+     * <p>Default FXML file path folder where every layout is located</p>
+     */
     private static final String FXML_FILE_PATH = "/it.polimi.ingsw/layout/login/";
+
+    /**
+     * <p>Path to the font used</p>
+     */
     private static final String FONT_PATH = "/it.polimi.ingsw/graphical.resources/font/SpecialElite-Regular.ttf";
+
+    /**
+     * The screen size
+     */
     private static final GraphicsDevice SCREEN_DEVICE = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+
+    /**
+     * the screen width
+     */
     private static final int SCREEN_WIDTH = SCREEN_DEVICE.getDisplayMode().getWidth();
+
+    /**
+     * The screen height
+     */
     private static final int SCREEN_HEIGHT = SCREEN_DEVICE.getDisplayMode().getHeight();
 
+    /**
+     * This method loads the font in the graphical user interface
+     */
     private static void loadMyShelfieFont() {
         Font.loadFont(LoginPage.class.getResourceAsStream(FONT_PATH), Font.getDefault().getSize());
     }
 
+    /**
+     * Retrieves the {@linkplain #FXML_FILE_PATH FXML layout folder}
+     *
+     * @return the FXML layout folder path as text
+     */
     public static String getFXMLFilePath() {
         return FXML_FILE_PATH;
     }
 
+    /**
+     * Given a "fxml" file name that contains a layout for a page of the game, this method retrieves the complete
+     * file-path to the layout file
+     *
+     * @param fileName the name of the layout
+     *
+     * @return the complete file-path to a file containing a layout page structure
+     */
     public static String getFXMLFile(String fileName) {
         return getFXMLFilePath() + fileName;
     }
 
+    /**
+     * @return the screen width
+     */
     public static int getScreenWidth() {
         return SCREEN_WIDTH;
     }
 
+    /**
+     * @return the screen height
+     */
     public static int getScreenHeight() {
         return SCREEN_HEIGHT;
     }
 
+    /**
+     * <p>This method can be used to construct a {@link Scene scene} from a
+     *    specific file containing the layout (.fxml)</p>
+     * <p>More precisely the scene retrieved by this method will have {@code height} and {@code width}
+     *    based on screen size and a percent passed as parameter</p>
+     *
+     * @param FXMLFileName the layout file reference
+     * @param percentWidth the percent width of the scene based on screen size
+     * @param percentHeight the percent height of the scene based on screen size
+     *
+     * @return the scene personalized as described
+     */
     public static Scene setUpScene(final String FXMLFileName, final double percentWidth, final double percentHeight) {
         loadMyShelfieFont();
 
@@ -55,6 +122,16 @@ public abstract class MyShelfieApplication extends Application{
         return scene;
     }
 
+    /**
+     * <p>This method can be used to construct a {@link Scene scene} from a
+     *    specific file containing the layout (.fxml)</p>
+     * <p>More precisely the scene retrieved by this method will have {@code height} and {@code width}
+     *    based on screen size (full screen)</p>
+     *
+     * @param FXMLFileName the layout file reference
+     *
+     * @return the scene personalized as described
+     */
     public static Scene setUpFullScreenScene(final String FXMLFileName) {
         loadMyShelfieFont();
 
@@ -71,6 +148,11 @@ public abstract class MyShelfieApplication extends Application{
         return scene;
     }
 
+    /**
+     * this method center a given {@link Stage stage} in the device screen
+     *
+     * @param stage the stage to center
+     */
     public static void centerStageInScreen(Stage stage) {
         stage.setX((Screen.getPrimary().getVisualBounds().getWidth() - stage.getWidth()) / 2);
         stage.setY((Screen.getPrimary().getVisualBounds().getHeight() - stage.getHeight()) / 2);
