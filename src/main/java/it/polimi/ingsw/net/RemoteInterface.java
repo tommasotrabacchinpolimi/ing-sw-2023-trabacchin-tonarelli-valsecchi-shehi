2be718f628e967e2d7e2ea925be4470ea292a77a
Emanuele Valsecchi <emanuele1.valsecchi@mail.polimi.it>
2023-05-19 11:14:33 +0200

    Added JavaDoc for ClientInterface.java, RemoteInterface.java and User.java Minor changes to ClientTest.java, RmiAccepter.java and SocketConnectionManager.java Minor changes to LobbyControllerInterface.java JavaDoc

--- src/main/java/it/polimi/ingsw/net/RemoteInterface.java
+++ src/main/java/it/polimi/ingsw/net/RemoteInterface.java
@@ -1,9 +1,16 @@
 package it.polimi.ingsw.net;
 
+import it.polimi.ingsw.controller.ClientInterface;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 /**
+ * <p>This interface defines methods that can be invoked from a non-local virtual machine, regardless that this is a
+ * client or a server</p>
+ *
+ * @see Remote
+ *
  * @author Tommaso Trabacchin
  * @author Melanie Tonarelli
  * @author Emanuele Valsecchi
@@ -13,5 +20,11 @@ import java.rmi.RemoteException;
  */
 
 public interface RemoteInterface extends Remote {
+
+    /**
+     * <p>This method is used to verify that the connection between server and client is still working.</p>
+     *
+     * @throws RemoteException in case of connection failure
+     */
     public void nop() throws RemoteException;
 }
