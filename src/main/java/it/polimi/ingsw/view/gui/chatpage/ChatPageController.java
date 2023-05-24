c98f7cc9d410115419c338cca7bc9f8e90152531
Adem Shehi <adem.shehi@mail.polimi.it>
2023-05-24 10:03:18 +0200

    Added ChatPageController.java

---
+++ src/main/java/it/polimi/ingsw/view/gui/chatpage/ChatPageController.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw.view.gui.chatpage;
+
+
+import it.polimi.ingsw.view.gui.MyShelfieController;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChatPageController extends MyShelfieController {
+    public AnchorPane mainContainer;
+    public GridPane pageMainDivider;
+    public GridPane chatAndControlsDivider;
+    public AnchorPane chatWoodContainer;
+    public GridPane chatElementsContainer;
+    public GridPane controlsDivider;
+    public GridPane playerListContainer;
+    public GridPane buttonContainer;
+    public ImageView icon2;
+    public ImageView icon3;
+    public ImageView icon1;
+    public StackPane UpperContainer;
+    public StackPane scrollAndTextContainer;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
