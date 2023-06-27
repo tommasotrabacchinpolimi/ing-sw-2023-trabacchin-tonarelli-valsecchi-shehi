5b188c35e494abc9babedd6019c04c4513ea2a24
Adem Shehi <adem.shehi@mail.polimi.it>
2023-06-27 14:33:13 +0200

    Documentation Changes in TextSizeChangeListener.java

--- src/main/java/it/polimi/ingsw/view/gui/TextSizeChangeListener.java
+++ src/main/java/it/polimi/ingsw/view/gui/TextSizeChangeListener.java
@@ -6,6 +6,12 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 
 /**
+ * This class implements a ChangeListener that adjusts the font size of text based on the size of a root pane.
+ *
+ * @author Tommaso Trabacchin
+ * @author Melanie Tonarelli
+ * @author Emanuele Valsecchi
+ * @author Adem Shehi
  *
  * @version 3.0
  * @since 27/05/2023
@@ -21,6 +27,13 @@ public class TextSizeChangeListener implements ChangeListener<Number> {
 
     private final double initialScale;
 
+    /**
+     * Constructs a TextSizeChangeListener object with the specified parameters.
+     *
+     * @param rootPane      The root pane containing the text.
+     * @param screenWidth  The width of the screen.
+     * @param screenHeight The height of the screen.
+     */
     public TextSizeChangeListener(final Pane rootPane, final double screenWidth, final double screenHeight) {
 
         this.rootPane = rootPane;
@@ -36,6 +49,13 @@ public class TextSizeChangeListener implements ChangeListener<Number> {
         this.initialScale = this.initialWidth / this.initialHeight;
     }
 
+    /**
+     * Called when the observed value changes. Adjusts the font size of the text based on the size of the root pane.
+     *
+     * @param observableValue The observable value.
+     * @param oldValue        The old value.
+     * @param newValue        The new value.
+     */
     @Override
     public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
 
