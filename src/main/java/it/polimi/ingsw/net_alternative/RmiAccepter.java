cba76ab3def786e6138942a4f96e16c47f75063b
Adem Shehi <adem.shehi@mail.polimi.it>
2023-06-27 11:12:07 +0200

    Javadoc Added for RmiAccepter.java

--- src/main/java/it/polimi/ingsw/net_alternative/RmiAccepter.java
+++ src/main/java/it/polimi/ingsw/net_alternative/RmiAccepter.java
@@ -5,18 +5,42 @@ import it.polimi.ingsw.controller.LobbyController;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-public class RmiAccepter extends UnicastRemoteObject implements RmiAccepterInterface{
+/**
+ * The {@code RmiAccepter} class is responsible for accepting RMI client connections and registering them with the server.
+ * It implements the {@link RmiAccepterInterface} and extends {@link UnicastRemoteObject} to enable remote method invocation.
+ *
+ * @author Tommaso Trabacchin
+ * @author Melanie Tonarelli
+ * @author Emanuele Valsecchi
+ * @author Adem Shehi
+ */
+public class RmiAccepter extends UnicastRemoteObject implements RmiAccepterInterface {
 
-    private OnServerConnectionLostListener onServerConnectionLostListener;
+    private final OnServerConnectionLostListener onServerConnectionLostListener;
+    private final ServerDispatcher serverDispatcher;
 
-    private ServerDispatcher serverDispatcher;
+    /**
+     * Constructs a {@code RmiAccepter} object with the specified parameters.
+     *
+     * @param onServerConnectionLostListener the listener to notify when a server connection is lost.
+     * @param serverDispatcher               the server dispatcher to handle incoming RMI client requests.
+     * @throws RemoteException if a remote exception occurs during object export.
+     */
     public RmiAccepter(OnServerConnectionLostListener onServerConnectionLostListener, ServerDispatcher serverDispatcher) throws RemoteException {
         super();
         this.onServerConnectionLostListener = onServerConnectionLostListener;
         this.serverDispatcher = serverDispatcher;
     }
 
-    public RmiServerInterface registerClient(RmiClientInterface clientSocket) throws RemoteException{
+    /**
+     * Registers the RMI client with the server by creating the necessary adapter and server instances.
+     * Additionally, a server heartbeater thread is started to monitor the client's connection status.
+     *
+     * @param clientSocket the RMI client interface object to register.
+     * @return the RMI server interface object to be used by the client.
+     * @throws RemoteException if a remote exception occurs during RMI operation.
+     */
+    public RmiServerInterface registerClient(RmiClientInterface clientSocket) throws RemoteException {
         ServerRmiAdapter serverRmiAdapter = new ServerRmiAdapter(clientSocket, onServerConnectionLostListener);
         RmiServerImpl rmiServer = new RmiServerImpl(serverDispatcher, serverRmiAdapter);
         ServerHeartBeater serverHeartBeater = new ServerHeartBeater(serverRmiAdapter, 1000, onServerConnectionLostListener);
