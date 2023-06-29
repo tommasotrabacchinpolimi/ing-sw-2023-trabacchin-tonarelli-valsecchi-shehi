a8d1f4040eae537486048bff12c42ba741e6f49a
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2023-06-29 19:18:27 +0200

    Added command line parameters support, and improved TUI

--- src/main/java/it/polimi/ingsw/AppServer.java
+++ src/main/java/it/polimi/ingsw/AppServer.java
@@ -24,22 +24,26 @@ public class AppServer {
      * @throws AlreadyBoundException  if the RMI object is already bound.
      */
     public static void main(String[] args) throws RemoteException, AlreadyBoundException {
-        if(args.length != 3) {
+        if(args.length != 5) {
             System.out.println("Parameters missing!");
             System.exit(-1);
         }
         int rmiPort = 0;
         int socketPort = 0;
+        int turnDuration = 0;
+        int timeOutDuration = 0;
         try{
             rmiPort = Integer.parseInt(args[1]);
             socketPort = Integer.parseInt(args[2]);
+            turnDuration = Integer.parseInt(args[3]);
+            timeOutDuration = Integer.parseInt(args[4]);
         } catch(NumberFormatException e) {
             System.out.println("Parameters are illegal!");
             System.exit(-1);
         }
         System.setProperty("java.rmi.server.hostname", args[0]);
-        System.setProperty("sun.rmi.transport.connectionTimeout", "5000");
-        LobbyController lobbyController = new LobbyController(1000*60);
+        System.setProperty("sun.rmi.transport.connectionTimeout", String.valueOf(timeOutDuration));
+        LobbyController lobbyController = new LobbyController(turnDuration);
         ControllerDispatcher controllerDispatcher = new ControllerDispatcher(lobbyController);
         ServerDispatcher serverDispatcher = new ServerDispatcher(controllerDispatcher);
         SocketAccepter socketAccepter = new SocketAccepter(serverDispatcher, socketPort, lobbyController);
